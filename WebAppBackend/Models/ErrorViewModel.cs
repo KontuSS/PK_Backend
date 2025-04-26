48ddcedc04d0e6759911a75ca6cc813b8f1a1871
Jakub Kałuziak <kontueses@gmail.com>
2025-04-26 12:25:53 +0200

    files fixes

--- WebAppBackend/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace WebAppBackend.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
