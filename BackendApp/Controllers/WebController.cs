3b2db0f05ba75ccc189bab95bb63bbea1febb561
Jakub Kałuziak <kontueses@gmail.com>
2025-05-06 18:29:22 +0200

    Added MVC

---
+++ BackendApp/Controllers/WebController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using BackendApp.Models;
+
+namespace BackendApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WebController : ControllerBase
+{
+
+}
