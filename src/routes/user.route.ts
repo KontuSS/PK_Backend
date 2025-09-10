97cd82b7750cf42df9ce4f7b60a9495917a7f1de
Przemyslaw Sek <przemek.sek@gmail.com>
2025-09-10 21:50:20 +0200

    fix: user profile routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,12 +1,12 @@
-import { Hono } from 'hono';
-import { UserController } from '../controllers/user.controllers.js';
-import { authMiddleware } from '../middlewares/auth.middleware.js';
+import { Hono } from "hono";
+import { UserController } from "../controllers/user.controllers.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const usersRoutes = new Hono();
 
-usersRoutes.use('*', authMiddleware);
+usersRoutes.use("*", authMiddleware);
 
-usersRoutes.get('/profile', UserController.getProfile);
-usersRoutes.post('/profile', UserController.updateProfile);
+usersRoutes.get("/profile", UserController.getProfile);
+usersRoutes.post("/profile/update", UserController.updateProfile);
 
 export default usersRoutes;
